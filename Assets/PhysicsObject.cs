ed0ae7fda2e77e26e8f27451b0ddfe4f0ffa1c56
TARTIGA <tima_ig@mail.ru>
2019-07-01 19:01:47 +0300

    add project

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement(move);
+    }
+
+    void Movement(Vector2 move)
+    {
+        rb2d.position = rb2d.position + move;
+    }
+}
